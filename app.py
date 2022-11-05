47a79655346d2add513b79c4822750712905b518
Ankur Dahal <adahal01@eecs.tufts.edu>
2022-11-05 16:08:47 -0400

    Implement auth with htpasswd

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, request
+from flask_htpasswd import HtPasswdAuth
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+app.config['FLASK_HTPASSWD_PATH'] = '.htpasswd'
+# app.config['FLASK_SECRET'] = 'Hey Hey Kids, secure me!'
 
-@app.route("/")
-def index():
-    return render_template('index.html') 
+htpasswd = HtPasswdAuth(app)
+
+
+@app.route('/')
+@htpasswd.required
+def index(user):
+    return 'Hello {user}'.format(user=user)
+
+# @app.route("/")
+# def index():
+#     return render_template('index.html') 
 
 @app.errorhandler(404)
 def page_not_found(e):
