b76dd10ff9a97232e8e46e717efe43af5a6c8fc0
Ankur Dahal <dahalankur123@gmail.com>
2022-11-05 14:20:44 -0400

    Remove htpasswd src

--- src/app.py
+++ src/app.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from flask_htpasswd import HtPasswdAuth
 
 app = Flask(__name__)
-app.config['FLASK_HTPASSWD_PATH'] = '../.htaccess'
-app.config['FLASK_SECRET'] = 'no secrets here!@#!@#' # TODO: get this from an env var later
-
-htpasswd = HtPasswdAuth(app)
 
 @app.route("/")
-@htpasswd.required
-def hello_world(user):
-    return f"<p>Hello, {user}!</p>"
-
-
+def hello_world():
+    return f"<p>Hello, world!</p>"
