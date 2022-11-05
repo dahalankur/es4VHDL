deda3a1df1fe727b34bc96e3f345d42586c5ee0c
Ankur Dahal <dahalankur123@gmail.com>
2022-11-05 14:07:49 -0400

    Experiment with htaccess

--- src/app.py
+++ src/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_htpasswd import HtPasswdAuth
 
 app = Flask(__name__)
+app.config['FLASK_HTPASSWD_PATH'] = '../.htaccess'
+app.config['FLASK_SECRET'] = 'no secrets here!@#!@#' # TODO: get this from an env var later
+
+htpasswd = HtPasswdAuth(app)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@htpasswd.required
+def hello_world(user):
+    return f"<p>Hello, {user}!</p>"
 
 
