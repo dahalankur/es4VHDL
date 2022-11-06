2b45ccc87480d6bc030b57f665c2c16ce1e21f6a
Ankur Dahal <adahal01@eecs.tufts.edu>
2022-11-05 21:45:23 -0400

    Add more endpoints to interact with files

--- app.py
+++ app.py
@@ -37,6 +37,43 @@ def make_tree(path):
 
 # TODO: Update the size of the file tree and the editor window to be correct.
 
+@app.route('/delete_file', methods = ['GET'])
+@htpasswd.required
+def delete_file(user):
+    path = os.path.expanduser(f'/h/{user}/.es4/')
+    to_delete = request.args.get('filename')
+    if os.path.exists(path=to_delete):
+        if not os.path.isfile(path=to_delete):
+            flash('Cannot delete non-file', 'error')
+            return redirect(url_for('index'))
+        # Delete the file
+        os.remove(to_delete)
+    else:
+        flash('No file with the given name exists', 'error')
+        return redirect(url_for('index'))
+    return render_template('index.html', tree=make_tree(path), file_contents=default_msg) 
+
+
+@app.route('/new_folder', methods = ['GET'])
+@htpasswd.required
+def new_folder(user):
+    path = os.path.expanduser(f'/h/{user}/.es4/')
+    current_dir = request.args.get('current_dir')
+    new_dir = current_dir + "/" + request.args.get('dirname')
+    
+    if os.path.exists(path=current_dir):
+        if os.path.exists(path=new_dir):
+            flash('The directory with the given name already exists', 'error')
+            return redirect(url_for('index'))
+        
+        # Create the new directory with appropriate permissions
+        os.mkdir(path=new_dir)
+        os.chmod(path=new_dir, mode=0o2775)
+    else:
+        flash('The path to the current directory does not exist', 'error')
+        return redirect(url_for('index'))
+    return render_template('index.html', tree=make_tree(path), file_contents=default_msg) 
+ 
 
 @app.route('/new_project', methods = ['GET'])
 @htpasswd.required
@@ -46,7 +83,7 @@ def new_project(user):
     print(projname)
     if not os.path.exists(path=projname):
         os.mkdir(path=projname)
-        os.chmod(path=projname, mode=0o2774)
+        os.chmod(path=projname, mode=0o2775)
     else:
         flash('The project already exists', 'error')
         return redirect(url_for('index'))
